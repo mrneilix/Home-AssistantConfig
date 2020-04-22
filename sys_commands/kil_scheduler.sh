ace5829d0888620750360fc64fa3e7596274c998
mrneilix <khanna.neil@gmail.com>
2020-04-22 16:48:30 -0400

    scheduled backup

--- sys_commands/kil_scheduler.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill -9 ` ps -ef | grep ./scheduler.sh | cut -d "a" -f 1`
--- sys_commands/su_log_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-var=$(grep 'sys_commands_path' '/config/secrets.yaml' | tail -n1); var=${var//sys_commands_path: /}; log_path=$var'/supervisor.log';
-
-ha supervisor logs &> $log_path
